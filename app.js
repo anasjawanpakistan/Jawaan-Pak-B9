e51a4d669638ee4b8eefdf494589b7c7bc52a60e
anasjawanpakistan <anasm0213@gmail.com>
2023-09-08 15:27:24 +0500

    js

---
+++ app.js
@@ -0,0 +1,102 @@
+// chapter No. 1
+
+// Q1
+alert("Hello")
+
+// Q2
+alert("Error! Please enter a valid password")
+
+// Q3
+alert("Welcome to JS Land \n Happy Coding!")
+
+// Q4
+alert("Welcome to JS Land")
+
+// Q5
+alert("Hello...I can run JS through my web brower's console")
+
+
+
+// Chapter No. 2
+
+// Q1
+var username = "Jhone Doe";
+
+alert(username)
+
+// Q2
+var myname = "MuhammadAnasNadeem"
+
+alert(myname)
+
+// Q3
+var message;
+message = "Hello World"
+alert(message)
+
+// Q4
+var JS = "message"
+var message = "Hello World"
+alert(JS + " " + message)
+
+
+
+// Q5
+
+
+var firstnamejhone ="Jhone Doe";
+var age ="20 years old";
+var newmessage ="Certified Web Developor"
+alert(firstnamejhone + " " + age + " " + newmessage)
+
+// Q6
+
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\np";
+
+alert(pizza)
+
+// Q7
+
+var email ="My email address is example@example.com";
+
+alert(email)
+
+
+// Q7
+
+var alert1 ="I am trying to learn from book of a easy way to learn Java Script";
+
+alert(alert1)
+
+// Q8
+
+document.write("Yah! I can write HTML through Java Script")
+
+
+
+
+
+// Chapter No.3
+
+// Q1
+var newage ="I am 20 years old";
+alert(newage)
+
+var visited1 ="You have visited this site 15 time";
+alert(visited1) 
+
+
+var visited2 ="My birth year is 2000\ndata type of my declared is number";
+alert(visited2)
+
+
+var visited3 ="John Doe ordered 5 T-shirt(s) on XYZ Clothing store";
+alert(visited3)
+
+
+
+
+// Chapter No.4
+
+// Q1
+
